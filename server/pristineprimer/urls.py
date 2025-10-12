65bcd8e32caabb5b4774de8bd94cb26eae31c725
“Chrispin <c43057772@gmail.com>
2025-10-12 23:30:35 +0300

    Fixed CORS issue

--- server/pristineprimer/urls.py
+++ server/pristineprimer/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,6 +9,8 @@ urlpatterns = [
     path('api/auth/', include('users.urls')),
     path('api/', include('properties.urls')),
     # path('dashboard/', include('dashboard.urls')),
+    
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='react-app'),
 ]
 
 if settings.DEBUG:
