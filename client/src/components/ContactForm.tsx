2319ee521f6aeae25b98edfcf6079d75df20434d
“Chrispin <c43057772@gmail.com>
2025-10-19 21:17:54 +0300

    Added correct pink colour for submit button

--- client/src/components/ContactForm.tsx
+++ client/src/components/ContactForm.tsx
@@ -272,7 +272,12 @@ const ContactForm = ({ propertyId, formType = 'inquiry', onSubmit }: ContactForm
         />
       </div>
 
-      <Button type="submit" variant="teal" size="lg" disabled={loading} className="w-full">
+      <Button 
+        type="submit" 
+        size="lg" 
+        disabled={loading} 
+        className="w-full bg-[#f77f77] border-[#f77f77] hover:bg-[#f56a6a] hover:border-[#f56a6a] disabled:bg-[#f9b4b4] disabled:border-[#f9b4b4]"
+      >
         {getSubmitButtonText()}
       </Button>
       
