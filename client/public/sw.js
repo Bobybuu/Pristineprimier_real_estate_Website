f6825c494ecf323619f7c6325b3223fb5442c44f
Chrispin Odiwuor <c43057772@gmail.com>
2025-12-06 19:51:43 +0300

    Added a new version of app

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,6 +1,6 @@
 // public/sw.js
-const CACHE_NAME = 'pristine-primier-v1.0.6';
-const STATIC_CACHE = 'pristine-primier-static-v1.0.6';
+const CACHE_NAME = 'pristine-primier-v1.0.7';
+const STATIC_CACHE = 'pristine-primier-static-v1.0.7';
 
 // Caching Static assets that don't change often
 const staticAssetsToCache = [
